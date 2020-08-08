d50513e70ce6f97af0e81a4e19317f27d189bbfe
Blessan Mathew <blessan.mathew@zalando.de>
2020-08-08 17:45:58 +0200

    Removes prism js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import "Assets/css/reset.css";
-import "Assets/css/syntax.css";
 import "Assets/css/markdown.css";
-import "prismjs/themes/prism.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
